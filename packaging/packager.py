41df0d66ba54d865b4a8c904c43f3ec9b7724aab
Kevin Van Hijfte <kevin.van.hijfte@openvstorage.com>
2017-10-23 13:30:41 +0200

    Option to upload and including hotfixes directly into correct release

--- packaging/packager.py
+++ packaging/packager.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     parser.add_option('-p', '--product', dest='product')
     parser.add_option('-r', '--release', dest='release', default=None)
     parser.add_option('-e', '--revision', dest='revision', default=None)
+    parser.add_option('-o', '--hotfix-release', dest='hotfix_release', default=None)
     parser.add_option('--no-rpm', dest='rpm', action='store_false', default=True)
     parser.add_option('--no-deb', dest='deb', action='store_false', default=True)
     options, args = parser.parse_args()
@@ -45,7 +46,7 @@ if __name__ == '__main__':
         # 2. Build & Upload packages
         if options.deb is True and 'deb' not in settings['repositories']['exclude_builds'].get(options.product, []):
             DebianPackager.package(metadata)
-            DebianPackager.upload(metadata, add=add_package)
+            DebianPackager.upload(metadata, add=add_package, hotfix_release=options.hotfix_release)
         if options.rpm is True and 'rpm' not in settings['repositories']['exclude_builds'].get(options.product, []):
             RPMPackager.package(metadata)
             RPMPackager.upload(metadata)  # add not relevant for RPM

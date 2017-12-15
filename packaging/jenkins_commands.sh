a5977c43f9f7f6bf3115560d1f7eeee32c6479e8
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2017-12-15 11:48:46 +0100

    Escape the bash variables

--- packaging/jenkins_commands.sh
+++ packaging/jenkins_commands.sh
@@ -5,13 +5,13 @@ find ${WORKSPACE} -name .coverage -exec rm {} \;
 
 PYTHONPATH=:${WORKSPACE}
 cd ${WORKSPACE}
-ARGS="--product ${product} --release ${release}"
-if [ -n "${revision}" ] ; then
-  ARGS="${ARGS} --revision ${revision} --hotfix-release ${hotfix_release}"
-fi
+ARGS="--product=""${product}"" --release=""${release}"
 if [ "${artifact_only}" == "true" ]
 then
   ARGS="${ARGS} --artifact-only"
+fi
+if [ -n "${revision}" ] ; then
+  ARGS="${ARGS} --revision=""${revision}"" --hotfix-release=""${hotfix_release}"
 fi
 
 python packaging/packager.py ${ARGS}
